ee1a748177cdc9171b372af2833d310da208112b
Sandip7677 <anuj7870040353@gmail.com>
2023-06-15 13:24:57 +0530

    updated type fix in App.js

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Phonebook from "./assignment/1/Index";
-import Gmail from "./assignment/2/Index";
+import Phonebook from "./Assignment/1/Index";
+import Gmail from "./Assignment/2/Index";
 
 export default function App() {
-	return (
-		// <View style={styles.container}>
-		//   <Text>Open up App.js to start working on your app!</Text>
-		//   <StatusBar style="auto" />
-		// </View>
-		// <Phonebook />
-		<Gmail />
-	);
+  return (
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    // <Phonebook />
+    <Gmail />
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
