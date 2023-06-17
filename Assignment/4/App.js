2e73a4c8fee425615211ff29f0cb07690a4eed84
Divyanshu Kumar <4674kumardivyanshu@gmail.com>
2023-06-17 16:43:51 +0530

    Assignment 4 : Attempt 1

---
+++ Assignment/4/App.js
@@ -0,0 +1,21 @@
+// In App.js in a new project
+
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./components/Home";
+
+const Stack = createNativeStackNavigator();
+
+function Mail() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Mail;
