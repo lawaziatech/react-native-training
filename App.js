aa3cc6d4a45d17c7c5bc529f098942ab6c5615a5
Mahimatestgithub <guptamahima7848@gmail.com>
2023-06-19 01:02:32 +0530

    assignment

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Contact from './assignment/1/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Contact/>
   );
 }
 
