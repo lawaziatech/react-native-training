9bf3c75c4070ac694c2e29b7c54916301f528581
Divyanshu Kumar <4674kumardivyanshu@gmail.com>
2023-06-18 22:51:20 +0530

    updated

--- App.js
+++ App.js
@@ -3,14 +3,12 @@ import { StyleSheet, Text, View } from "react-native";
 /*import Contacts from './Assignment/1/Index';
 import Gmail_Clone from './Assignment/2/App';
 import Counter_App from './Assignment/3/App';*/
-// import FlatMAil from './Assignment/4/components/Home';
 import Mail from "./Assignment/4/App";
 export default function App() {
   return (
     //<Contacts/>
     //<Gmail_Clone/>
     // <Counter_App/>
-    //  <FlatMAil/>
     <Mail />
   );
 }
