adf5aebd2e142e9ac3f8c8f529295653eb31cc8c
Mahimatestgithub <guptamahima7848@gmail.com>
2023-06-20 01:14:38 +0530

    msg

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-// import Contact from './assignment/1/index';
+ import Contact from './assignment/1/index';
 // import Email from './assignment/2/App';
-import CounterApp from './assignment/3/App';
+//import CounterApp from './assignment/3/App';
+import Mail from './assignment/4/App';
 export default function App() {
   return (
-    // <Contact/>
+     <Contact/>
     // <Email/>
-    <CounterApp/>
+   // <CounterApp/>
+   //<Mail/>
   )
 }
 
