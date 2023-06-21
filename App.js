98411fd0025987c9d0ee3eefa16c9baf8fb4160b
kaushalgupta45 <kkg8873408825@gmail.com>
2023-06-21 22:06:38 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,89 +1,46 @@
 import * as React from 'react';
-import { Text, View, StyleSheet,Image } from 'react-native';
+import { Text, View, StyleSheet,Image} from 'react-native';
 import Constants from 'expo-constants';
-
+import Mail from "./components/mail"
+import Footer from "./components/Footer"
+// You can import from local files
 
 
 export default function App() {
-  return (<View>
-    
-    <View style={{backgroundColor:"green",borderRadius:10}}>
-         <Text style={styles.paragraph}>Contacts</Text>  
-    </View>
-
+  return (
     <View>
       <View style={styles.container}>
-      <Image source={require("./assets/user.png")} style={styles.image}/>
-     <View>
-      <Text style={styles.text1} >
-      Kaushal Kumar
-      </Text>
-       <Text style={styles.text2} >
-      9123464284
-      </Text>
-     </View>
+        <Image source={require("./assets/menu.png")}/>
+        <Text style={{flex:1,paddingLeft:10}}>Search in Emails</Text>
+        <Image source={require("./assets/profile.png")}/>
+      </View>
+
+      <Mail logo={"S"} name={"Superset"} heading={"Avoid these resume mistakes and La..."} msg={"Hey there, 75% of resumes never get re..."}  time={"7.31 PM"}/>
+
+      <Mail logo={"I"} name={"Internshala"} heading={"New Interships for you.."} msg={"hello, Kaushal here is the list of internships.."}  time={"7.05 PM"}/>
+
+      <Mail logo={"I"} name={"Internshala"} heading={"New Interships for you.."} msg={"hello, Kaushal 5 new internships of you.."}  time={"6:32 PM"}/>
+
+      <Mail logo={"L"} name={"Linkedin"} heading={"New message in Inbox"} msg={"hello, Kaushal someone messaged you.."}  time={"6:08 PM"}/>
+      <Mail logo={"L"} name={"Linkedin"} heading={"New connection request"} msg={"hello, Kaushal new connection request for you.."}  time={"5:56 PM"}/>
+
+      <Mail logo={"I"} name={"Internshala"} heading={"New Interships for you.."} msg={"hello, Kaushal 5 new internships of you.."}  time={"5:23 PM"}/>
       
-    </View>
+      <Footer/>
 
 
-    <View style={styles.container}>
-      <Image source={require("./assets/user.png")} style={styles.image}/>
-     <View>
-      <Text style={styles.text1} >
-      Jeesman
-      </Text>
-       <Text style={styles.text2} >
-      6578901234
-      </Text>
-     </View>
-      
     </View>
-
-    <View style={styles.container}>
-      <Image source={require("./assets/user.png")} style={styles.image}/>
-     <View>
-      <Text style={styles.text1} >
-      Rahul Singh
-      </Text>
-       <Text style={styles.text2} >
-      8863542404
-      </Text>
-     </View>
-      
-    </View>
-
-    <View style={styles.container}>
-      <Image source={require("./assets/user.png")} style={styles.image}/>
-     <View>
-      <Text style={styles.text1} >
-      Shreya
-      </Text>
-       <Text style={styles.text2} >
-      9765342543
-      </Text>
-     </View>
-      
-    </View>
-    </View>
-    
-    
-  </View>);
+  );
 }
 
 const styles = StyleSheet.create({
- container:{
-   flexDirection:"row",justifyContent:"flex-start",padding:20,backgroundColor:"#F8E8EE",width:300,marginLeft:6,borderRadius:10,marginTop:10,shadowColor:"#5C8984",shadowOffset:{height:2,width:5}
-
- },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+  container: {
+    flexDirection:"row",
+    justifyContent:"space-around",
+    padding:5,
+    borderRadius:10,
+    margin:3,
+    backgroundColor:"#E3F4F4"
   },
-  image:{
-    borderWidth:2,borderRadius:100,borderColor:"#5C8984",height:40,width:40
-  },
-  text1:{marginTop:8,marginLeft:30,fontSize:20},
-  text2:{marginTop:4,marginLeft:30,fontSize:15,color:"#5C8984"},
 });
+
